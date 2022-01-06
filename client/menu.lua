54eeface0ae33913673af85fefeac6061c83f44a
Froot Loops <94587874+FrootLoopsBowl@users.noreply.github.com>
2022-01-06 06:41:45 +0100

    Remove ESX

--- client/menu.lua
+++ client/menu.lua
@@ -1,17 +1,3 @@
-ESX = nil
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-
-	while ESX.GetPlayerData().job == nil do
-		Citizen.Wait(10)
-	end
-
-	PlayerData = ESX.GetPlayerData()
-end)
-
 local isOpen = false
 local checkbox = {
     one = true
